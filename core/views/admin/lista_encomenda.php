4831debc4c1eb0b6105d9df45a0fefabe3893316
Edenilson João Pereira <ninocriptocoin@gmail.com>
2022-10-31 00:00:06 -0300

    Update lista_encomenda.php

--- core/views/admin/lista_encomenda.php
+++ core/views/admin/lista_encomenda.php
@@ -5,10 +5,37 @@
         <div class="col-md-3">
             <?php include_once(__DIR__.'/layouts/admin_menu.php'); ?>
         </div>
-        
-        <div class="col-md-9 text-center">
-            <h3>Lista de encomendas: <?=$filtro!=''?$filtro:''?></h3>
-            <hr>
+<!-- DESAFIO FEITO PELO PROFESSOR - apresentar o nome do filtro selecionado no OPTION do SELECT.
+    A forma atual q ESTÁ, foi a q eu desenvolvi! abaixo vai estar  a forma q o professor explicou!
+php
+if(isset($_GET['f'])){
+    $f = $_GET['f'];
+}
+//colocar(operador ternário) dentro do OPTION
+<option value="" $f==''?'selected':''; ></option>$f==''?'selected':'';
+<option value="pendente" $f=='pendente'?'selected':'pendente';>Pendentes</option>
+-->
+
+        <div class="col-md-9">
+            <div class="row">
+                <div class="col-5 text-center">
+                    <label class="btn btn-sm btn-outline-primary" for="escolher_status">Escolher Estado:</label><br>
+                    <select name="escolher_status" id="escolher_status" onchange="definir_filtro()">
+                        <option value="<?=$filtro?>"><?=$filtro?></option>
+                        <option value="pendente">Pendentes</option>
+                        <option value="processamento">Em Processamento</option>
+                        <option value="enviada">Enviadas</option>
+                        <option value="cancelada">Canceladas</option>
+                        <option value="concluida">Concluídas</option>
+                    </select>
+                </div>
+                <div class="col-3  text-center">
+                    <h4>Lista de encomendas: <?=$filtro!=''?$filtro:''?></h4>
+                </div>
+                <div class="col-4 text-center align-self-center">
+                    <a class="btn btn-sm btn-outline-primary" href="?a=lista_encomenda">Todas as Encomendas</a>
+                </div>
+            </div><hr>
             <?php if(count($lista_encomenda)==0):?>
                 <p><hr>Não existe encomendas registradas<hr></p>
             <?php else: ?>
@@ -40,15 +67,11 @@
         </div>
     </div>
 </div>
-<!-- <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script> -->
-<!--------------------------------------------------------------------------------------------
-VIA CDN baixado agora
-------------------------------------------------------------------------------------------------->
-<!-- <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.12.1/datatables.min.js"></script> -->
-
+<!-- define o DATATABLE para a tabela com o ID=tabelaecomenda -->
 <script>
 $(document).ready( function () {
     $('#tabelaecomenda').DataTable({
+        //define a LINGUAGEM(tradução) das propriedades da DataTable
         language:{
                 "decimal":        ",",
                 "emptyTable":     "No data available in table",
@@ -76,4 +99,9 @@ $(document).ready( function () {
             }
         });
 } );
+function definir_filtro(){
+    var filtro = document.getElementById('escolher_status').value;
+    //Reload da pagina com o determinado filtro
+    window.location.href = window.location.pathname+"?"+$.param({'a':'lista_encomenda','f': filtro});
+}
 </script>
